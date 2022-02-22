505feff6a01db39ac448ccf0085b26732cc72a16
Meenakshi0710 <95168217+Meenakshi0710@users.noreply.github.com>
2022-02-22 11:48:50 +0530

    capital first letter

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 function Alert(props) {
 
-   {/* const capitalize = (word)=>{
+    const capitalize = (word)=>{
         const lower = word.toLowerCase();
         console.log(lower.charAt(0).toUpperCase() + lower.slice(1));
         return lower.charAt(0).toUpperCase() + lower.slice(1);
         
-    } */}
+    } 
     return (
         props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-  <strong>{props.alert.type}</strong>: {props.alert.msg} 
+  <strong>{capitalize(props.alert.type)}</strong> : {props.alert.msg} 
   
 </div>
     );
